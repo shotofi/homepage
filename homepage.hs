f00e53b254c9e6bd2fa427984550dc41b01452c6
Jussi Vesala <jussi.vesala@iki.fi>
2011-12-30 23:09:13 +0200

    add kuvat9

--- homepage.hs
+++ homepage.hs
@@ -26,9 +26,9 @@ main = hakyll $ do
     match (list ["main.html", "harjoitukset.html", "alkeiskurssi.html", "leirit.html", 
             "yleistietoa.html", "muistoja.html", "tyylikuvaus.html", "katat.html", 
             "muumaailma.html", "kds_saannot.html", "karate_all.html", 
-            "kuvia_09.html", "kuvia_08.html", "kuvia_07.html", "kuvat1.html", 
+            "kuvia_09.html", "kuvia_08.html", "kuvia_07.html", "kuvat1.html",
             "kuvat2.html", "kuvat3.html", "kuvat4.html", "kuvat5.html", 
-            "kuvat6.html", "kuvat7.html", "kuvat8.html"]) $ do
+            "kuvat6.html", "kuvat7.html", "kuvat8.html", "kuvat9.html", "kuvat10.html"]) $ do
         route   $ setExtension "html"
         compile $ readPageCompiler
             >>> applyTemplateCompiler "templates/template.html"

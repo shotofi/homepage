5dfea6f0da7594ce19a423c33aab312b0059252f
Jussi Vesala <jussi.vesala@iki.fi>
2011-12-30 16:42:31 +0200

    add alkeiskurssi page

--- homepage.hs
+++ homepage.hs
@@ -15,7 +15,7 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateCompiler
 
-    match (list ["main.html", "harjoitukset.html"]) $ do
+    match (list ["main.html", "harjoitukset.html", "alkeiskurssi.html"]) $ do
         route   $ setExtension "html"
         compile $ readPageCompiler
             >>> applyTemplateCompiler "templates/template.html"

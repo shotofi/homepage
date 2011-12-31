9429b921184fbb4c0bccf9b505e84b88353de807
Jussi Vesala <jussi.vesala@iki.fi>
2011-12-31 12:16:11 +0200

    add muutokset page, minor fixes also

--- homepage.hs
+++ homepage.hs
@@ -23,10 +23,9 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateCompiler
     
-    match "galleries.html" $ compile pageCompiler
-
+    -- Tavalliset sivut, joissa voi olla yksi, kaksi tai kolme palstaa
     match (list ["main.html", "harjoitukset.html", "alkeiskurssi.html", "leirit.html", 
-            "yleistietoa.html","tyylikuvaus.html", "katat.html", 
+            "yleistietoa.html","tyylikuvaus.html", "katat.html", "muutokset.html",
             "muumaailma.html", "kds_saannot.html", "karate_all.html",
             "yhteystiedot.html"]) $ do
         route   $ setExtension "html"
@@ -34,7 +33,8 @@ main = hakyll $ do
             >>> applyTemplateCompiler "templates/template.html"
             >>> relativizeUrlsCompiler
 
-    -- Galleria-sivut, joihin tulee vasempaan laitaan lista gallerioista
+    -- Galleria-sivut, joihin tulee vasempaan laitaan lista gallerioista ja loppusivun levyinen 
+    -- toinen palsta
     match (list ["muistoja.html", "kuvia_09.html", "kuvia_08.html", "kuvia_07.html", "kuvat1.html",
             "kuvat2.html", "kuvat3.html", "kuvat4.html", "kuvat5.html", 
             "kuvat6.html", "kuvat7.html", "kuvat8.html", "kuvat9.html", "kuvat10.html"]) $ do

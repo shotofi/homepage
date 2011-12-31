410cab13cb8959e2b592cda4f8b940d5e1b6dc96
Jussi Vesala <jussi.vesala@iki.fi>
2011-12-31 11:55:38 +0200

    use gallery-snippet on left navigation

--- homepage.hs
+++ homepage.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>))
+import Control.Arrow (arr, (>>>))
 
 import Hakyll
 
@@ -22,15 +22,24 @@ main = hakyll $ do
         compile copyFileCompiler
 
     match "templates/*" $ compile templateCompiler
+    
+    match "galleries.html" $ compile pageCompiler
 
     match (list ["main.html", "harjoitukset.html", "alkeiskurssi.html", "leirit.html", 
-            "yleistietoa.html", "muistoja.html", "tyylikuvaus.html", "katat.html", 
-            "muumaailma.html", "kds_saannot.html", "karate_all.html", 
-            "kuvia_09.html", "kuvia_08.html", "kuvia_07.html", "kuvat1.html",
-            "kuvat2.html", "kuvat3.html", "kuvat4.html", "kuvat5.html", 
-            "kuvat6.html", "kuvat7.html", "kuvat8.html", "kuvat9.html", "kuvat10.html",
+            "yleistietoa.html","tyylikuvaus.html", "katat.html", 
+            "muumaailma.html", "kds_saannot.html", "karate_all.html",
             "yhteystiedot.html"]) $ do
         route   $ setExtension "html"
         compile $ readPageCompiler
+            >>> applyTemplateCompiler "templates/template.html"
+            >>> relativizeUrlsCompiler
+
+    -- Galleria-sivut, joihin tulee vasempaan laitaan lista gallerioista
+    match (list ["muistoja.html", "kuvia_09.html", "kuvia_08.html", "kuvia_07.html", "kuvat1.html",
+            "kuvat2.html", "kuvat3.html", "kuvat4.html", "kuvat5.html", 
+            "kuvat6.html", "kuvat7.html", "kuvat8.html", "kuvat9.html", "kuvat10.html"]) $ do
+        route   $ setExtension "html"
+        compile $ readPageCompiler
+            >>> applyTemplateCompiler "templates/gallery.html"
             >>> applyTemplateCompiler "templates/template.html"
             >>> relativizeUrlsCompiler

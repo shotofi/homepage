1a147f09acb98ac9d8b98b926b5413810410f71c
Jussi Vesala <jussi.vesala@iki.fi>
2012-06-07 23:42:14 +0300

    utility methods ver2

--- Git.hs
+++ Git.hs
@@ -6,6 +6,7 @@ import Data.Data
 import Data.Typeable
 import Data.List
 import Data.Ord
+import Data.Monoid
 import System.FilePath
 import System.Locale (TimeLocale, defaultTimeLocale)
 import Data.Time.Format (parseTime, formatTime)
@@ -18,8 +19,8 @@ gitDate :: String -> IO String
 gitDate path = do
   dir <- getCurrentDirectory
   let absPath = dir ++ "/" ++ path
-  dateString <- readDate absPath
-  return $ defaultFormat dateString
+  date' <- readDate absPath
+  return $ defaultFormat date'
 
 defaultFormat :: Maybe UTCTime -> String
 defaultFormat = formatTime' "%d.%m.%Y %R"
@@ -40,27 +41,29 @@ readDate file = do
 -------------------
 data HtmlPage = HtmlPage { absPath :: String, title:: String, url :: String, date :: Maybe UTCTime} deriving (Data, Typeable, Show)
 
-fileInfo :: String -> String-> String -> IO HtmlPage
-fileInfo path title url = do 
-  date <- readDate path
-  return $ HtmlPage path title url date
-
-createUrl :: HtmlPage -> String
-createUrl page = "<a href=\"" ++ url' ++ "\">" ++ link ++ "</a>"
-  where url'   = (url page)
-        title' = (title page)
-        date'  = defaultFormat $ (date page)
-        link   = title' ++ " (" ++ date' ++ ")"  
-  
 content :: String -> String -> IO [HtmlPage]
 content basedir htmldir = do
   let dir = basedir ++ "/" ++ htmldir
   paths <- getDirectoryContents dir
   let files = filter (isSuffixOf "html") paths
   mapM (\x -> fileInfo (dir ++ "/" ++ x) x x) files
+
+fileInfo :: String -> String-> String -> IO HtmlPage
+fileInfo absPath title url = do 
+  date <- readDate absPath
+  return $ HtmlPage absPath title url date
+
+createUrl :: HtmlPage -> String
+createUrl page = "<a href=\"" ++ url' ++ "\">" ++ link ++ "</a>"
+  where url'   = (url page)
+        title' = (title page)
+        date'  = defaultFormat $ (date page)
+        link   = title' ++ " (" ++ date' ++ ")"
   
 pageList pages = do 
   page <- pages
-  let sorted = sortBy (comparing date) page
-  return (reverse $ map createUrl sorted)
+  let sorted = reverse $ sortBy (comparing date `mappend` comparing title) page
+      urls   = map createUrl sorted
+      lis    = map (\x -> "<li>" ++ x ++ "</li>") urls      
+  return ("<ul>" ++ (join lis) ++ "</ul>")
   

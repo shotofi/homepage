a5cd0add961c7bead4e002b13277134d4a29a360
Jussi Vesala <jussi.vesala@iki.fi>
2012-12-04 20:26:03 +0200

    initial index with new layout

--- hs/homepage.hs
+++ hs/homepage.hs
@@ -22,80 +22,41 @@ main = hakyllWith config $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "js/*" $ do
+    match "js/**" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
+    match "css/**" $ do
         route   idRoute
-        compile compressCssCompiler
+        compile copyFileCompiler
 
     match "templates/*" $ compile templateCompiler
     
-    -- Tavalliset sivut, joissa voi olla yksi, kaksi tai kolme palstaa
-    match (predicate (\i -> matches "pages/*.html" i && not (matches "pages/*_en.html" i))) $ do
+    match "snippets/*" $ compile myPageCompiler
+    
+    match "pages/index.html" $ do
         route setRoot
         compile $ readPageCompiler
             >>> addDefaultFields
             >>> arr (setGitValues)
+            >>> requireA "snippets/tapahtumakalenteri.html" (setFieldA "leftup" $ arr pageBody)
+            >>> requireA "snippets/alkeiskurssi-mainos.html" (setFieldA "leftdown" $ arr pageBody)
+            >>> requireA "snippets/harjoitusajat.html" (setFieldA "rightup" $ arr pageBody)
+            >>> arr (setField "rightdown" "")
+            >>> applyTemplateCompiler "templates/three-column.html"
             >>> applyTemplateCompiler "templates/template.html"
-            >>> relativizeUrlsCompiler
-
-    -- Galleria-sivut, joihin tulee vasempaan laitaan lista gallerioista ja loppusivun levyinen 
-    -- toinen palsta
-    match (predicate (\i -> matches "pages_gallery/*.html" i && not (matches "pages_gallery/*_en.html" i))) $ do
-        route setRoot
-        compile $ readPageCompiler
-            >>> addDefaultFields
-            >>> arr (setGitValues)
-            >>> applyTemplateCompiler "templates/gallery.html"
-            >>> applyTemplateCompiler "templates/template.html"
-            >>> relativizeUrlsCompiler
-
-    -- ENGLISH VERSION
-    match (predicate (\i -> matches "pages/*_en.html" i)) $ do
-        route setRoot
-        compile $ readPageCompiler
-            >>> addDefaultFields
-            >>> arr (setGitValues)
-            >>> applyTemplateCompiler "templates/template_en.html"
-            >>> relativizeUrlsCompiler
-
-    match (predicate (\i -> matches "pages_gallery/*_en.html" i)) $ do
-        route setRoot
-        compile $ readPageCompiler
-            >>> addDefaultFields
-            >>> arr (setGitValues)
-            >>> applyTemplateCompiler "templates/gallery_en.html"
-            >>> applyTemplateCompiler "templates/template_en.html"
-            >>> relativizeUrlsCompiler
-
-    -- CHANGES PAGES
-    match "pages_changes/muutokset.html" $ do 
-        route setRoot
-        compile $ readPageCompiler
-            >>> addDefaultFields
-            >>> arr (setGitValues)            
-            >>> arr (setChanges)        
-            >>> applyTemplateCompiler "templates/changes.html"
-            >>> applyTemplateCompiler "templates/template.html"
-            >>> relativizeUrlsCompiler
-
-    match "pages_changes/muutokset_en.html" $ do 
-        route setRoot
-        compile $ readPageCompiler
-            >>> addDefaultFields
-            >>> arr (setGitValues)            
-            >>> arr (setChanges)        
-            >>> applyTemplateCompiler "templates/changes.html"
-            >>> applyTemplateCompiler "templates/template_en.html"
             >>> relativizeUrlsCompiler
 
     
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration
-  { deployCommand = "scp -r _site/* shotofi@shoto.fi:public_html/"
+  { deployCommand = "scp -r _site/* shotofi@shoto.fi:public_html_test/"
   }
+
+myPageCompiler :: Compiler Resource (Page String)
+myPageCompiler = readPageCompiler
+  >>> addDefaultFields
+  >>> arr applySelf
 
 setRoot :: Routes
 setRoot = customRoute stripTopDir

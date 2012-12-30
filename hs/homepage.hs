b955dc5701b236f87ff5cf8bad088e758e821970
Jussi Vesala <jussi.vesala@iki.fi>
2012-12-30 12:24:05 +0200

    change language js added

--- hs/homepage.hs
+++ hs/homepage.hs
@@ -29,7 +29,8 @@ main = hakyllWith config $ do
     match "templates/*" $ compile templateCompiler
     match "menus/*"     $ compile snippetCompiler
     match "snippets/*"  $ compile snippetCompiler
-    
+
+    -- FINNISH SITE --
     match "pages/index.html" $ do
         route setRoot
         compile $ historyReadPageCompiler
@@ -78,7 +79,6 @@ main = hakyllWith config $ do
             >>> requireA "menus/menu-kuvia.html" (setFieldA "left" $ arr pageBody)
             >>> fiTwoColumnCompiler
 
-    -- CHANGES PAGES
     match "pages/muutokset.html" $ do
         route setRoot
         compile $ historyReadPageCompiler
@@ -86,6 +86,63 @@ main = hakyllWith config $ do
             >>> requireA "menus/menu-lisatietoa.html" (setFieldA "left" $ arr pageBody)
             >>> applyTemplateCompiler "templates/changes.html"
             >>> fiTwoColumnCompiler
+
+    -- ENGLISH SITE --
+    match "pages/index_en.html" $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "snippets/tapahtumakalenteri_en.html" (setFieldA "leftup" $ arr pageBody)
+            >>> requireA "snippets/alkeiskurssi-mainos_en.html" (setFieldA "leftdown" $ arr pageBody)
+            >>> requireA "snippets/harjoitusajat-mainos_en.html" (setFieldA "rightup" $ arr pageBody)
+            >>> arr (setField "rightdown" "")
+            >>> enThreeColumnCompiler
+
+    forM_ ["pages/harjoittelu_en.html", "pages/harjoittelupaikat_en.html", "pages/katat_en.html", "pages/perustekniikka_en.html",
+           "pages/salietiketti_en.html", "pages/graduointi_en.html", "pages/tyylikuvaus_en.html"] $ \p ->
+      match p $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "menus/menu-harjoittelu_en.html" (setFieldA "left" $ arr pageBody)
+            >>> enTwoColumnCompiler
+
+    match "pages/leirit_en.html" $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "menus/menu-harjoittelu_en.html" (setFieldA "leftup" $ arr pageBody)
+            >>> requireA "snippets/leirit-mainos_en.html" (setFieldA "rightup" $ arr pageBody)
+            >>> arr (setField "leftdown" "")
+            >>> arr (setField "rightdown" "")
+            >>> enThreeColumnCompiler
+
+    forM_ ["pages/alkeiskurssi_en.html", "pages/alkeiskurssi-ilmo_en.html", "pages/alkeiskurssi-ilmo-ok_en.html"] $ \p ->
+      match p $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "snippets/alkeiskurssi-ohjelma_en.html" (setFieldA "right" $ arr pageBody)
+            >>> enTwoColumnRightCompiler
+
+    forM_ ["pages/yhteystiedot_en.html", "pages/muutseurat_en.html", "pages/karate_all_en.html",
+           "pages/jasenmaksut_en.html", "pages/saannot_en.html"] $ \p ->
+      match p $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "menus/menu-lisatietoa_en.html" (setFieldA "left" $ arr pageBody)
+            >>> enTwoColumnCompiler
+
+    forM_ ["pages/kuvia_en.html", "pages/kuvia_07_en.html", "pages/kuvia_08_en.html", "pages/kuvia_09_en.html"] $ \p ->
+      match p $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> requireA "menus/menu-kuvia_en.html" (setFieldA "left" $ arr pageBody)
+            >>> enTwoColumnCompiler
+
+    match "pages/muutokset_en.html" $ do
+        route setRoot
+        compile $ historyReadPageCompiler
+            >>> arr (setChanges)
+            >>> requireA "menus/menu-lisatietoa_en.html" (setFieldA "left" $ arr pageBody)
+            >>> applyTemplateCompiler "templates/changes_en.html"
+            >>> enTwoColumnCompiler
 
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration
@@ -102,6 +159,7 @@ historyReadPageCompiler = readPageCompiler
   >>> addDefaultFields
   >>> arr (setGitValues)
 
+-- Finnish compilers
 fiTwoColumnCompiler :: Compiler (Page String) (Page String)
 fiTwoColumnCompiler = applyTemplateCompiler "templates/two-column.html"
   >>> applyTemplateCompiler "templates/template.html"
@@ -115,6 +173,22 @@ fiTwoColumnRightCompiler = applyTemplateCompiler "templates/two-column2.html"
 fiThreeColumnCompiler :: Compiler (Page String) (Page String)
 fiThreeColumnCompiler = applyTemplateCompiler "templates/three-column.html"
   >>> applyTemplateCompiler "templates/template.html"
+  >>> relativizeUrlsCompiler
+
+-- English compilers
+enTwoColumnCompiler :: Compiler (Page String) (Page String)
+enTwoColumnCompiler = applyTemplateCompiler "templates/two-column.html"
+  >>> applyTemplateCompiler "templates/template_en.html"
+  >>> relativizeUrlsCompiler
+
+enTwoColumnRightCompiler :: Compiler (Page String) (Page String)
+enTwoColumnRightCompiler = applyTemplateCompiler "templates/two-column2.html"
+  >>> applyTemplateCompiler "templates/template_en.html"
+  >>> relativizeUrlsCompiler
+
+enThreeColumnCompiler :: Compiler (Page String) (Page String)
+enThreeColumnCompiler = applyTemplateCompiler "templates/three-column.html"
+  >>> applyTemplateCompiler "templates/template_en.html"
   >>> relativizeUrlsCompiler
 
 setRoot :: Routes
--- js/shoto.js
+++ js/shoto.js
@@ -1,3 +1,47 @@
+
+function makeLanguageChanger() {
+  function englishPage() { return $('html').attr('lang') === 'en'}
+  function finnishPage() { return $('html').attr('lang') === 'fi'}
+  function rootPage() {return window.location.pathname == "/"}
+
+  function parseEnglishUrl() {
+    if(rootPage()) {
+      return "index_en.html"
+    } else {
+      var pathname = window.location.pathname
+      return pathname.slice(0, -5) + "_en.html"
+    }
+  }
+
+  function parseFinnishUrl() {
+    var pathname = window.location.pathname
+    return pathname.slice(0, -8) + ".html"
+  }
+
+  function navigateToFinnishPage() {
+    if(englishPage()) {
+      window.location.pathname = parseFinnishUrl()
+    }
+  }
+
+  function navigateToEnglishPage() {
+    if(finnishPage()) {
+      window.location.pathname = parseEnglishUrl()
+    }
+  }
+
+  return {
+    navigateToFinnishPage: navigateToFinnishPage,
+    navigateToEnglishPage: navigateToEnglishPage
+  }
+}
+
+
 $(window).load(function() {
   $('.flexslider').flexslider()
+  var languageChanger = makeLanguageChanger()
+
+  $('#finnish').click(languageChanger.navigateToFinnishPage)
+  $('#english').click(languageChanger.navigateToEnglishPage)
+
 })
